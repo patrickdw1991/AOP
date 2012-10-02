88542e0daeb00ffe5882b1bfa330d3dee3c75a80
Patrick de Wit <pasta480@gmail.com>
2012-10-02 10:42:30 +0200

    initial version

---
+++ JadeAOP/JadeAOP/src/BagageRobotAgent.java
@@ -0,0 +1,13 @@
+import jade.core.Agent;
+import jade.core.AID;
+
+public class BagageRobotAgent extends Agent {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6314097892473530932L;
+
+	protected void setup() {
+		System.out.println("Hello! "+getAID().getName()+" is ready.");
+		}
+}
